3717156bc8dd9448b648548a1b75defddac493f1
lucasdeabril <lucaspassosdesouza08@gmail.com>
2023-11-17 17:15:05 -0400

    rotas liberadas pelo cors

--- demo/verify-node.js
+++ demo/verify-node.js
@@ -23,6 +23,12 @@ app.get('/', (req, res) =>{
     
 });
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 app.post('/teste', (req, res) =>{
     // return response.json({messege:'Sever is up'});
